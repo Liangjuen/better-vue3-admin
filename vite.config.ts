6000e5bb96b717cdae96de96102acdd703d09631
梁举恩 <3103421940@qq.com>
2024-05-23 00:15:20 +0800

    chore(custom): add pinia-plugin-persistedstate package

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
 		resolve: {
 			// 别名
 			alias: {
-				'@': resolve(''),
+				'~': resolve(''),
 				'@config': resolve('config'),
 				'@network': resolve('network'),
 				'@modules': resolve('modules'),
