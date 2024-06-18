275e25a2e797536fa308149f925e416a8d6419c9
梁举恩 <3103421940@qq.com>
2024-06-18 14:32:15 +0800

    perf: router store update ...

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,3 +1,46 @@
 import { RouteRecordRaw } from 'vue-router'
 
-export const staticRoutes: RouteRecordRaw[] = []
+export const routes: RouteRecordRaw[] = [
+	{
+		path: '/',
+		name: 'Layout',
+		component: () => import('~/views/main/index.vue'),
+		children: []
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: () => import('~/views/login/index.vue'),
+		meta: {
+			label: '登录'
+		}
+	},
+	{
+		path: '/401',
+		name: 'Unauthorized',
+		component: () => import('~/views/error/401.vue'),
+		meta: {
+			label: '401'
+		}
+	},
+	{
+		path: '/403',
+		name: 'Forbidden',
+		component: () => import('~/views/error/403.vue'),
+		meta: {
+			label: '403'
+		}
+	},
+	{
+		path: '/404',
+		name: 'NotFound',
+		component: () => import('~/views/error/404.vue'),
+		meta: {
+			label: '404'
+		}
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		component: () => import('~/views/error/404.vue')
+	}
+]
