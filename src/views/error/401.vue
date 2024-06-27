60ce773a9bb04c04183d79411eaec4fa3ef4aec1
Liangjuen <3103421940@qq.com>
2024-06-27 23:01:07 +0800

    feature: 新增异常模块

--- src/views/error/401.vue
+++ src/views/error/401.vue
@@ -1,7 +1,5 @@
 <template>
-	<error-page :code="401" desc="认证失败，请重新登录！" />
+	<b-exception-page :code="401" desc="认证失败，请重新登录！" />
 </template>
 
-<script setup lang="ts">
-import ErrorPage from './components/error-page.vue'
-</script>
+<script setup lang="ts"></script>
