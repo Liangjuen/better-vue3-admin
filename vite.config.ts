e42844f2b9db215ca4784b664a634eae246cc6c6
Liangjuen <3103421940@qq.com>
2024-07-11 04:47:21 +0800

    chore: 构建优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import path from 'node:path'
-import { defineConfig, UserConfig, ConfigEnv, loadEnv } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { visualizer } from 'rollup-plugin-visualizer'
+import VitePluginCompression from 'vite-plugin-compression'
 import vue from '@vitejs/plugin-vue'
 
 function resolve(dir: string) {
@@ -11,14 +13,13 @@ function resolve(dir: string) {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
-	const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig((): UserConfig => {
 	return {
 		plugins: [
 			vue(),
 			// 组件自动引入配置
 			/**
-			 * 参考文档: https://github.com/unplugin/unplugin-auto-import#readme
+			 * doc: https://github.com/unplugin/unplugin-auto-import#readme
 			 */
 			AutoImport({
 				resolvers: [ElementPlusResolver()],
@@ -27,7 +28,7 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
 				dts: './typings/auto-imports.d.ts'
 			}),
 			/**
-			 * 参考文档: https://github.com/unplugin/unplugin-vue-components#readme
+			 * doc: https://github.com/unplugin/unplugin-vue-components#readme
 			 */
 			Components({
 				resolvers: [ElementPlusResolver()],
@@ -36,6 +37,9 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
 				directoryAsNamespace: true,
 				dts: './typings/components.d.ts'
 			}),
+			/**
+			 * doc: https://github.com/anncwb/vite-plugin-svg-icons/tree/master/#readme
+			 */
 			createSvgIconsPlugin({
 				// 指定需要缓存的图标文件夹
 				iconDirs: [path.resolve(process.cwd(), 'src/assets/icons/svg')],
@@ -57,6 +61,21 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
 						}
 					]
 				}
+			}),
+			/**
+			 * doc: https://github.com/btd/rollup-plugin-visualizer
+			 */
+			visualizer({ open: true }),
+			/**
+			 * doc: https://github.com/anncwb/vite-plugin-compression/tree/master/#readme
+			 */
+			VitePluginCompression({
+				verbose: true, // 是否在控制台中输出压缩结果
+				disable: false,
+				threshold: 10240, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+				algorithm: 'gzip', // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
+				ext: '.gz',
+				deleteOriginFile: false // 源文件压缩后是否删除
 			})
 		],
 		resolve: {
@@ -64,6 +83,15 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
 			alias: {
 				'-': resolve(''),
 				'~': resolve('src')
+			}
+		},
+		build: {
+			rollupOptions: {
+				output: {
+					chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
+					entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
+					assetFileNames: '[ext]/[name]-[hash].[ext]' // 资源文件像 字体，图片等
+				}
 			}
 		}
 	}
